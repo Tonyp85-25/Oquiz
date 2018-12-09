52228e3305127074cdff514fc89e44302d3f36e7
Tonyp85-25 <anthony.paulin@enil-mamirolle.fr>
2018-12-09 22:43:50 +0100

    View basepath modified3

--- src/Controllers/CoreController.php
+++ src/Controllers/CoreController.php
@@ -22,8 +22,8 @@ abstract class CoreController{
         // Je définis des données utiles pour toutes les templates
         $this->templates->addData([
             'title' => 'Oquiz', // => $title
-            'basePath' => BASE_PATH,
-            // 'basePath' => $_SERVER['BASE_URI'] ?:'', // => $basePath
+            
+            'basePath' => $_SERVER['BASE_URI'] ?:'', // => $basePath
             'router' => $this->router, // => $router
             'connectedUser' => User::getUser()  // => $connectedUser
         ]);
