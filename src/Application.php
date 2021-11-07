500ba4bd97dfdce523f248243db280a05e96a80a
Tonyp85-25 <anthony-paulin@etudiant.oclock.io>
2021-11-07 14:54:05 +0100

    Application and core controller upadated

--- src/Application.php
+++ src/Application.php
@@ -1,69 +1,72 @@
 <?php
 namespace Oquiz;
+
 use \AltoRouter;
 
-class Application {
+class Application
+{
     public $router;
-  public function __construct()
-  {
-    // Création du routeur
-    $this->router = new AltoRouter();
-    // On ignore une partie de l'URL
-    // On récupère donc la partie de l'URL qui
-    // est fixe grâce à $_SERVER['BASE_URI']
-    $basePath = isset($_SERVER['BASE_URI']) ? $_SERVER['BASE_URI'] : '';
-    // On renseigne la partie de l'URL fixe
-    $this->router->setBasePath($basePath);
-    // On lance le mapping
-    $this->mapping();
-  }
-  private function mapping(){
-    // On mappe toutes nos URL
-    // La page d'accueil
-    $this->router->map('GET', '/', ['MainController', 'home'], 'home');
-    $this->router->map('GET', '/quiz/[i:id]', ['QuizController', 'quiz'], 'quiz');
-    $this->router->map('POST', '/quiz/[i:id]', ['QuizController', 'quizPost'], 'quiz_post');
-    $this->router->map('GET', '/compte/[i:id]', ['UserController', 'profile'], 'profile');
-    $this->router->map('GET', '/signin/', ['UserController', 'signin'], 'signin');
-    $this->router->map('POST', '/signin/', ['UserController', 'signinPost'], 'signin_post');
-    $this->router->map('GET', '/signout/', ['UserController', 'signout'], 'signout');
-    $this->router->map('GET|POST', '/signup/', ['UserController', 'signup'], 'signup');
-
-
-
-
+    private $container;
+    public function __construct()
+    {
+        // Création du routeur
+        $this->router = new AltoRouter();
+        // On ignore une partie de l'URL
+        // On récupère donc la partie de l'URL qui
+        // est fixe grâce à $_SERVER['BASE_URI']
+        $basePath = isset($_SERVER['BASE_URI']) ? $_SERVER['BASE_URI'] : '';
+        // On renseigne la partie de l'URL fixe
+        $this->router->setBasePath($basePath);
+        // On lance le mapping
+        $this->mapping();
+    }
+    private function mapping()
+    {
+        // On mappe toutes nos URL
+        // La page d'accueil
+        $this->router->map('GET', '/', ['MainController', 'home',['QuizRepository']], 'home');
+        $this->router->map('GET', '/quiz/[i:id]', ['QuizController', 'quiz'], 'quiz');
+        $this->router->map('POST', '/quiz/[i:id]', ['QuizController', 'quizPost'], 'quiz_post');
+        $this->router->map('GET', '/compte/[i:id]', ['UserController', 'profile'], 'profile');
+        $this->router->map('GET', '/signin/', ['UserController', 'signin'], 'signin');
+        $this->router->map('POST', '/signin/', ['UserController', 'signinPost'], 'signin_post');
+        $this->router->map('GET', '/signout/', ['UserController', 'signout'], 'signout');
+        $this->router->map('GET|POST', '/signup/', ['UserController', 'signup'], 'signup');
     }
 
-  public function run () {
-      // Je récupère les données de Altorouter
-      $match = $this->router->match();
-      // print_r($match);
-      if (!$match) {
-          // On a pas trouvé la route, on indique le nouveau chemin
-          // $controller = new \Oquiz\Controllers\MainController();
-          // $controller->error404();
-          $controllerName = "\Oquiz\Controllers\MainController";
-          $methodName = 'error404';
-      }
-      else {
-          // Je regarde quel controller et quelle
-          // méthode je dois exécuter
-          // On pense à ajouter le namespace devant
-          // le nom de la classe et à échapper le
-          // dernier "\" pour ne pas perturber notre code
-          $controllerName = "\Oquiz\Controllers\\" . $match['target'][0];
-          $methodName = $match['target'][1];
-      }
-      // J'exécute la bonne et méthode
-      // $controller = new Oquiz\Controllers\MainController();
-      $controller = new $controllerName( $this->router );
-      // On en profite pour transmettre les paramètres
-      // contenus dans notre URL (si il y en a)
-      $controller->$methodName( $match['params'] );
-  }
+    public function run()
+    {
+        // Je récupère les données de Altorouter
+        $match = $this->router->match();
+        dump($match);
+        // print_r($match);
+        if (!$match) {
+            // On a pas trouvé la route, on indique le nouveau chemin
+            // $controller = new \Oquiz\Controllers\MainController();
+            // $controller->error404();
+            $controllerName = "\Oquiz\Controllers\MainController";
+            $methodName = 'error404';
+        } else {
+            // Je regarde quel controller et quelle
+            // méthode je dois exécuter
+            // On pense à ajouter le namespace devant
+            // le nom de la classe et à échapper le
+            // dernier "\" pour ne pas perturber notre code
+            $controllerName = "\Oquiz\Controllers\\" . $match['target'][0];
+            $methodName = $match['target'][1];
+        }
+
+        // J'exécute la bonne et méthode
+        // $controller = new Oquiz\Controllers\MainController();
+        $controller = new $controllerName($this->router);
+        // On en profite pour transmettre les paramètres
+        // contenus dans notre URL (si il y en a)
+        $controller->$methodName($match['params']);
+    }
 
     // Getter plus précis pour la propriété config
-    public function getConfig($key) {
+    public function getConfig($key)
+    {
         // Si $key existe dans $this->config
         if (array_key_exists($key, $this->config)) {
             // Je ne retourne pas toute la propriété config
@@ -72,11 +75,4 @@ class Application {
         }
         return false;
     }
-
-
-
-
-
-
-
 }
--- src/Controllers/CoreController.php
+++ src/Controllers/CoreController.php
@@ -11,8 +11,11 @@ abstract class CoreController{
     // Je stocke l'objet AltoRouter dans une propriété de la classe pour que ce soit accessible à toutes ses méthodes (et tous ses enfants)
     protected $router;
 
-  // $app = Application passé en paramètre lors du "dispatch"
-    public function __construct($router) {
+    protected $entityManager;
+
+    // $app = Application passé en paramètre lors du "dispatch"
+    public function __construct($router)
+    {
         // Je crée une instance du moteur de Templates
         $this->templates = new \League\Plates\Engine(ABS_BASE_PATH.'/src/Views');
 
@@ -27,27 +30,28 @@ abstract class CoreController{
             'router' => $this->router, // => $router
             'connectedUser' => User::getUser()  // => $connectedUser
         ]);
-       
     }
 
 
-     // Méthode permettant d'afficher un résultat sous forme de JSON
+    // Méthode permettant d'afficher un résultat sous forme de JSON
     // (utile quand la page est appelée via Ajax)
-    public static function sendJSON($data) {
+    public function sendJSON($data)
+    {
         header('Content-Type: application/json');
         echo json_encode($data, JSON_PRETTY_PRINT);
         exit;
     }
 
     // Méthode permettant de rediriger vers une URL passée en paramètre
-    public function redirect($url) {
+    public function redirect($url)
+    {
         header('Location: '.$url);
         exit;
     }
 
-// Méthode permettant de rediriger vers une route de l'application
-    public function redirectToRoute($routeName, $params=array()) {
+    // Méthode permettant de rediriger vers une route de l'application
+    public function redirectToRoute($routeName, $params=array())
+    {
         $this->redirect($this->router->generate($routeName, $params));
     }
-
 }
